a9cd77279930919f387455da29ee2c84c8d3b90d
F70st <damir.serazetdinow@yandex.ru>
2025-01-18 18:57:50 +0300

    Все разрешённые хосты и скрыт блок конвертации для не админов

--- card/settings.py
+++ card/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&xifg%6vcm3!#rfa1y0-a176yv_qyb(4tlexh5uw=c3+b=sm6)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
