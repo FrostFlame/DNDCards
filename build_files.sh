1deb1133659f25d02468d676b8827ebdd65ea18d
F70st <damir.serazetdinow@yandex.ru>
2025-01-18 22:17:32 +0300

    Подготовка к деплою на Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,16 @@
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 # build_files.sh
 pip install -r requirements.txt
 
 # make migrations
 python manage.py migrate
 python manage.py collectstatic
+
+echo "BUILD END"
